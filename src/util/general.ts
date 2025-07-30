b6a24cdfad7ddca68c7db8617b63349e658df29c
Fjell Project <info@getfjell.com>
2025-07-30 10:45:19 -0700

    Restore published Fjell dependency versions and remove unused registry parameter from remove operation
    - Switch @fjell/core, @fjell/lib, @fjell/logging, @fjell/registry, and @fjell/eslint-config dependencies in package.json and docs/package.json from local file: links back to published npm versions
    - Update @fjell/docs-template in docs/package.json to use npm version ^1.0.24 instead of a local file link
    - Remove registry argument from getRemoveOperation and related call in Operations.ts, as it is no longer required
    - Eliminate unused Library import in src/ops/remove.ts for cleaner dependencies
    - Standardize ESLint disable comments at the top of remove.ts and general.ts for broader lint rule suppression
    - Ensure files end with a newline character to maintain formatting consistency

--- src/util/general.ts
+++ src/util/general.ts
@@ -1,5 +1,5 @@
- 
-/* eslint-disable no-undefined */
+
+/* eslint-disable */
 export const clean = (obj: any) => {
   return Object.fromEntries(
     Object.entries(obj).filter(([_, v]) => v !== undefined)
